f0c0bd0eb9cc810d7e7e0b352c9d6da5f1965921
Dylane Mempouza <dylanemempouza02@gmail.com>
2024-01-09 16:34:17 +0100

    app structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import LeftSideBar from "../components/Shared/LeftSideBar";
+import MainContainer from "../components/Shared/MainContainer";
+import RigthSideBar from "../components/Shared/RigthSideBar";
+import TopBar from "../components/Shared/TopBar";
+import BottomBar from "../components/Shared/BottomBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,15 +14,25 @@ export const metadata: Metadata = {
   description: "Social Media application",
 };
 
-export default function RootLayout({
+export default function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider >
+    <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className}  `}>
+          <div className='flex flex-row'>
+            <LeftSideBar />
+              <MainContainer>
+                <TopBar />
+                {children}
+              </MainContainer>
+            <RigthSideBar />
+          </div>
+          <BottomBar />
+        </body>
       </html>
     </ClerkProvider>
   );
